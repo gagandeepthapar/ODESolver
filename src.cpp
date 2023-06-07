c52db1b4c5adce3af60ebd0256288d0de7689efa
gaganthapar12309 <gsthapar@gmail.com>
2023-06-07 14:08:39 -0700

    adding structure for ode solvers

---
+++ src.cpp
@@ -0,0 +1,30 @@
+#include "src.hpp"
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// RKF-45 Definitions
+RKF45::RKF45(vector<double>(*func)(float, vector<double>),
+             vector<float> tspan,
+             vector<double> y0):
+            
+             m_func(func),
+             m_t0(tspan[0]),
+             m_tF(tspan[1]),
+             m_y0(y0){
+  solve();
+}
+
+void RKF45::solve(){
+  for(int i = 0; i < (int)m_tF; i++){
+    t_hist.push_back(i);
+  }
+}
+
+vector<double> RKF45::step(){
+
+  vector<double> data = {1.0f, 2.0f};
+  return data;
+}
---
+++ src.hpp
@@ -0,0 +1,48 @@
+#ifndef SRC_HPP
+#define SRC_HPP
+
+#include <vector>
+
+using namespace std;
+
+// Abstract Base Class 
+class ODESolver{
+
+  virtual void solve() = 0;  
+  virtual vector<double> step() = 0;
+
+};
+
+class RKF45:ODESolver{
+ 
+  // variables
+  public:
+    vector<float> t_hist;
+    vector< vector<double> > state_hist;
+
+  private:
+    vector<double>(*m_func)(float, vector<double>);
+    vector<double> m_y0;
+    float m_t0;
+    float m_tF;
+
+  // methods
+  public:
+    // constructor
+    RKF45(vector<double>(*func)(float, vector<double>),
+        vector<float> tspan,
+        vector<double> y0);
+
+    // destructor
+
+  private:
+    // run solver for given parameters
+    void solve() override;
+
+    // step
+    vector<double> step() override;
+
+};
+
+#endif // !SRC_HPP
+
